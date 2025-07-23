03acadc3d9110ef754d789746213359377cfc0a6
Syedameher90 <syedameherirfan@gmail.com>
2025-07-23 09:41:24 -0700

    new file

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
   <>
-  <h1 className="text-2xl text-rose-400 font-extrabold bg-red-200 text-center w-full h-14 p-3">hello world</h1>
+  <div className="text-2xl text-rose-400 font-extrabold bg-red-200 text-center w-full h-14 p-3">hello world</div>
   </>
   )
 }
