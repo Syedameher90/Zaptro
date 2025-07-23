77832c65f363ba6bcb8e828d489030fddf1dc307
Syedameher90 <syedameherirfan@gmail.com>
2025-07-23 06:42:26 -0700

    change file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react"
-
 function App() {
 
   return (
   <>
-  <h1>hello world</h1>
+  <h1 className="text-2xl text-rose-400 font-extrabold bg-red-200 text-center w-full h-14 p-3">hello world</h1>
   </>
   )
 }
