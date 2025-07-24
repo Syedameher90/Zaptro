27401f5f6d8e6d3cc7ef5839933cb3ca91d3a1e8
Syedameher90 <syedameherirfan@gmail.com>
2025-07-24 13:09:12 -0700

    new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
 import React from "react"
 import icon from "../src/assets/react.svg"
-import Products from "./Pages/Products"
+import Home from "./Pages/Home.jsx"
+import { BrowserRouter, Routes, Route } from "react-router"
+import Products from "./Pages/Products.jsx"
+import About from "./Pages/About.jsx"
+import Contact from "./Pages/Contact.jsx"
+
 function App() {
 
   return (
-  <>
-  <div className="flex justify-between bg-blue-950 text-center w-full h-18 p-3">
-    <div className="">
-      <img src={icon} alt="icon" className="h-12" />
-    </div>
-      <ul className="flex justify-center items-center gap-6">
-        <li className="text-white cursor-pointer font-bold">Home</li>
-        <li className="text-white cursor-pointer font-bold">About</li>
-        <li className="text-white cursor-pointer font-bold">Contact</li>
-        <li className="text-white cursor-pointer font-bold">Publisher</li>
-      </ul>
-      <div className="mx-2">
-     <Products name="Login"/>
-</div>
-  </div>
-  </>
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home/>}></Route>
+    <Route path="/products" element={<Products/>}></Route>
+    <Route path="/about" element={<About/>}></Route>
+    <Route path="/contact" element={<Contact/>}></Route>
+
+  </Routes>
+  </BrowserRouter>
   )
 }
 
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Products = (props) => {
   return (
-    <button className="px-8 py-2 bg-sky-400 text-white rounded-full" >
-      {props.name}
-      </button>
+   <></>
   )
 }
 
