4833084f40e797a71eba78c85034504ff34991ca
Syedameher90 <syedameherirfan@gmail.com>
2025-08-01 01:01:18 -0700

    new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,18 @@
 import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router"
-import Home from "./Pages/Home.jsx"
-import Products from "./Pages/Products.jsx"
-import About from "./Pages/About.jsx"
-import Contact from "./Pages/Contact.jsx"
-import Navbar from "./components/Navbar.jsx"
+import { Route } from "react-router"
+import { Routes } from "react-router"
+import { BrowserRouter } from "react-router"
+import Home from './Pages/Home.jsx'
+
 
 function App() {
 
   return (
-  <BrowserRouter>
-  <Navbar/>
-  <Routes>
-    <Route path="/" element={<Home/>}></Route>
-    <Route path="/products" element={<Products/>}></Route>
-    <Route path="/about" element={<About/>}></Route>
-    <Route path="/contact" element={<Contact/>}></Route>
-
-  </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
