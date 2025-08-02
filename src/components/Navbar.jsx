fd3d146d4f013588c220d28e7c5a3b418bd0e90e
Syedameher90 <syedameherirfan@gmail.com>
2025-08-02 13:14:58 -0700

    constraction on website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,20 @@
+import { MapPin } from 'lucide-react'
 import React from 'react'
-import logo from "../assets/react.svg"
+import { FaCaretDown } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 const Navbar = () => {
+  const location = false
   return (
-    <div className='bg-white py-3 shadow-xl'>
+    <div className=' bg-white py-3 shadow-xl'>
       <div className="max-w-6xl mx-auto flex justify-between items-center">
         {/* Logo section */}
-        <div className="">
-          <Link></Link>
+        <div className="flex">
+          <Link to={'/'}><h1 className='font-bold text-3xl '><span className='text-red-500 font-serif text-6xl'>S</span>yedStore</h1></Link>
+      <div className="flex items-center gap-1 cursor-pointer text-gray-700">
+        <MapPin className='text-red-500'/>
+        <span className="font-semibold">{location ? <div></div> :"Add Address"}</span>
+        <FaCaretDown/>
+       </div>
         </div>
       </div>
     </div>
