b7cafbc3f31f24aded8b12157e0a73420ba96376
Syedameher90 <syedameherirfan@gmail.com>
2025-08-21 08:32:01 -0700

    constraction on website

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,6 +3,7 @@ import { DataContext } from '../Context/DataContext'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 
 const Carousel = () => {
 
@@ -11,13 +12,33 @@ const Carousel = () => {
   useEffect(()=>{
     fetchAllProducts()
   },[])
-
+      const SamplePrevArrow = ({className, style, onClick}) => {
+    return(
+       <div onClick={onClick} className={`arrow ${className}`} style={{zIndex:3}}>
+        <AiOutlineArrowLeft className='arrows' style={{...style, display:"block", borderRadius: "50px", background:"#f53347", color: "white", position:"absolute", padding:"2px",left:"50px", }} onMouseOver="this.style.backgroundColor='#555'"/>
+       </div>
+    )
+  }
+ 
+    const SampleNextArrow = ({className, style, onClick}) => {
+    return(
+       <div onClick={onClick} className={`arrow ${className}`} style={{zIndex:3}}>
+        <AiOutlineArrowRight className='arrows' style={{...style, display:"block", borderRadius: "50px", background:"#f53347", color: "white", position:"absolute", padding:"2px",right:"50px", }} />
+       </div>
+    )
+  } 
    var settings = {
-    dots: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 20000,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    pauseOnHover: false,
+    nextArrow: <SampleNextArrow to="next"/>,
+    prevArrow: <SamplePrevArrow to="prev"/>,
+
   };
   return (
 
@@ -30,15 +51,18 @@ const Carousel = () => {
                 <h3 className="text-red-500 font-semibold font-sans text-sm">Powering Your World with the Best in Electronics</h3>
               <h1 className="text-4xl font-bold uppercase line-clamp-3 md:w-[500px] text-white">{item.title}</h1>
               <p className="md:w-[500px] line-clamp-3 text-gray-400 pr-7">{item.description}</p>
+              <button className="bg-gradient-to-r from-red-500 to-purple-500 text-white px-3 py-2 rounded-md cursor-pointer mt-2">Shop Now</button>
               </div>
+               <div>
+              <img src={item.image} alt={item.title} className="rounded-full w-[550px] hover:scale-105 transition-all shadow-2xl shadow-red-400" />
             </div>
+            </div>
+           
           </div>
 
         })
       }
-        <div>
-        <h3>1</h3>
-      </div>
+     
     </Slider>
   )
 }
